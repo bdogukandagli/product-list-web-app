1ef088897f5ac3f1d37b64a7b9d0514326176cc0
Burak Doğukan Dağlı <bdogukandagli@gmail.com>
2021-05-17 01:42:29 +0300

    constants & packages refactored

---
+++ client/src/utils/constant.js
@@ -0,0 +1,12 @@
+module.exports = {
+  sortTypes: {
+    priceLowtoHigh: 0,
+    priceHightoLow: 1,
+    newtoOld: 2,
+    oldtoNew: 3,
+  },
+  quantityActiontypes: {
+    decrement: 0,
+    increment: 1,
+  },
+};
